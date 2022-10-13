949845119b709536fa278016701d4bab73a82125
annguyen <annguyen.it.utc@gmail.com>
2022-10-13 13:04:24 +0700

    alpha-beta prune

--- ai.py
+++ ai.py
@@ -8,48 +8,54 @@ class AI:
         self.board = board
         self.depth = depth
 
-    def generateDecision(self) -> int:
-        return self.maximumPlayer(self.board.copy(), self.depth)
+    def generateDecision(self) -> list[int, int]:
+        return self.maximumPlayer(self.board.copy(), self.depth, -99999, 99999)
 
-    def maximumPlayer(self, board: Board, depth: int) -> list[int, int]:
+    def maximumPlayer(self, board: Board, depth: int, alpha: int, beta: int) -> list[int, int]:
         score = self.score(board.data)
         if depth == 0:
             return [None, score]
 
         _, cols = board.data.shape
         # Column, score
-        max = [None, -99999]
+        maxValue = [None, -99999]
 
         for col in range(cols):
             new_board = board.copy()
             if not new_board.is_valid_location(col):
                 continue
             new_board.drop_piece(col, 2)
-            next_move = self.minimizePlayer(new_board, depth - 1)
-            if max[0] == None or next_move[1] > max[1]:
-                max[0] = col
-                max[1] = next_move[1]
-        return max
+            next_move = self.minimizePlayer(new_board, depth - 1, alpha, beta)
+            if next_move[1] > maxValue[1]:
+                maxValue[0] = col
+                maxValue[1] = next_move[1]
+            if maxValue[1] >= beta:
+                break
+            alpha = max(alpha, maxValue[1])
+        return maxValue
 
-    def minimizePlayer(self, board: Board, depth: int) -> list[int, int]:
+    def minimizePlayer(self, board: Board, depth: int, alpha: int, beta: int) -> list[int, int]:
         score = self.score(board.data)
         if depth == 0:
             return [None, score]
 
         _, cols = board.data.shape
         # Column, score
-        min = [None, 99999]
+        minValue = [None, 99999]
 
         for col in range(cols):
             new_board = board.copy()
             if not new_board.is_valid_location(col):
                 continue
             new_board.drop_piece(col, 1)
-            next_move = self.maximumPlayer(new_board, depth - 1)
-            if min[0] == None or next_move[1] < min[1]:
-                min[0] = col
-                min[1] = next_move[1]
-        return min
+            next_move = self.maximumPlayer(new_board, depth - 1, alpha, beta)
+            if next_move[1] < minValue[1]:
+                minValue[0] = col
+                minValue[1] = next_move[1]
+            if minValue[1] <= alpha:
+                break
+            beta = min(beta, minValue[1])
+        return minValue
 
     def score(self, data: ndarray[float64]) -> int:
         rows, cols = data.shape
